721ad12589804e6ffd004e80bc4c5ba11d1bd381
dam824 <damiendagory@gmail.com>
2025-03-23 23:48:27 +0100

    update icon map broken after deploy

--- urbantrack-frontend/src/pages/TrajetDetail.vue
+++ urbantrack-frontend/src/pages/TrajetDetail.vue
@@ -5,6 +5,19 @@ import axios from 'axios'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
 
+// fix markers manquants après build Docker
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+
+//chemins casses en prod on vire
+delete (L.Icon.Default.prototype as any)._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+})
+
 // Types
 interface Arret {
   nom: string
